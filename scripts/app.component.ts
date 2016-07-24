f2d85ba7bb6a18e8e88b6d42a8970ca4a5c02d6e
vishal.tahil <vishal.tahil@hotmail.com>
2016-07-24 14:41:27 -0500

    Updating for azure buid

--- scripts/app.component.ts
+++ scripts/app.component.ts
@@ -22,7 +22,7 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
+    <h2>My Updated Heroes</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
